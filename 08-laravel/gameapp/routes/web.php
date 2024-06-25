6a24cef46ce656f243910f8c557a04e378b527dc
Laurab <lauravblanco93@gmail.com>
2024-06-24 20:27:47 -0500

    se agrega nuevos conocimientos en clase

--- 08-laravel/gameapp/routes/web.php
+++ 08-laravel/gameapp/routes/web.php
@@ -1,44 +1,24 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route ::get('/game/list', function() {
-    $games = App\Models\Game::all();
-    //var_dump($games);
-    dd($games->toArray());
+Route::get('catalogue', function () {
+    return view('catalogue');
+    });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//Route ::get('/game/{$id}', function($id) {
-//    $game = App\Models\Game::find($id);
-//    dd($game->toArray());
-// });
-
-Route ::get('/games/', function() {
-    $games = App\Models\Game::all();
-    return view('listgames')->with('games',$games);
-});
-
-Route ::get('/users/challenge', function() {
-    $users = App\Models\User::limit(20)->get();
-    //dd($users->toArray());
-    $code = "<table style='border-collapse;margin-inline: auto; font-family: Arial'>
-    <tr>
-    <th style='background: gray; color: white; padding: 0.4rem'>Id</th>
-    <th style='background: gray; color: white; padding: 0.4rem'>Fullname</th>
-    <th style='background: gray; color: white; padding: 0.4rem'>Age</th>
-    <th style='background: gray; color: white; padding: 0.4rem'>Created At</th>
-    </tr>";
-    foreach($users as $user) {
-        $code .= "<tr>";
-        $code .=    "<td style='border: 1px solid gray; padding: 0.4'>".$user->id."</td>";
-        $code .=    "<td style='border: 1px solid gray; padding: 0.4'>".$user->fullname."</td>";
-        $code .=    "<td style='border: 1px solid gray; padding: 0.4'>".Carbon\Carbon::parse($user->birthdate)->age."years old</td>";
-        $code .=    "<td style='border: 1px solid gray; padding: 0.4'>".$user->created_at->diffForHumans()."</td>";
-        $code .= "<tr>";
-    }
-    return $code . "</table>";
-});
+require __DIR__.'/auth.php';
